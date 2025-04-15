9198b9f3dbac6b1272d33f8a7c02943b48715de6
Diana Valeria Borja Sarmiento <valeria10diana@gmail.com>
2025-04-15 09:03:32 -0500

    Add files via upload

--- src/controllers/MetodosBusqueda.java
+++ src/controllers/MetodosBusqueda.java
@@ -1,1 +1,45 @@
-showConsole.views.ShowConsole;
+package controllers;
+import views.ShowConsole;
+
+public class MetodosBusqueda {
+    private ShowConsole showcConsole;
+    private Person [] people;
+
+    public MetodosBusqueda(Person [] persons){
+        showcConsole = new ShowConsole();
+        this.people = persons;
+        showPersonByCode();
+
+    }
+
+    public int busquedaLineal(int[] arreglo, int valorBuscado) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] == valorBuscado)
+                return i; 
+        }
+        return -1;
+    }
+
+    public int findPersonByCode (int code){
+        int cont = 0;
+        for (Person person : people) {
+            if (){
+                return cont;
+            }
+            cont ++;
+        }
+        
+        return -1;
+    }
+
+    public void showPersonByCode (int code){
+        int codeToFind = showcConsole.getCode();
+        int indexPerson = findPersonByCode(codeToFind);
+        if (indexPerson){
+            showcConsole.showMessage ("Persona encontrada");
+            showcConsole.showMessage (people[indexPerson]);
+        } else {
+            showcConsole.showMessage("Persona con codigo " + codeToFind + " no encontrado. ")
+        }
+    }
+}
--- src/views/ShowConsole.java
+++ src/views/ShowConsole.java
@@ -1,5 +1,16 @@
 package views;
 
 public class ShowConsole {
+    private Scanner scanner = new Scanner (System.in);
+    
+    public void showMenu(){
+        System.out.println("Metodo Busqueda: ")
+    }
+    public int getCode(){
+        System.out.println("Ingrese codigo: ")
+        int codigo = scanner.nextInt();
+        System.out.println("Codigo Ingresado: " + codigo);
+        return codigo;
+    }
     
 }
